68445c2dff77c79e71569e715034705da0505ce3
Raj <rajsanghavi9@gmail.com>
2021-08-13 14:42:52 +0530

    Added 'LUX' bootstrap theme and react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import { Container } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <>
+      {/* <div className="container">
+        <h1>E-Commerce App</h1>
+      </div> */}
+      {/* is equivalent to below */}
+      <Container>
+        <h1>E-Commerce App</h1>
+      </Container>
       <h1>E-Commerce App</h1>
     </>
   );
