938a3ee526d200b2d4506919ca8f873774cc90ee
Raj <rajsanghavi9@gmail.com>
2021-08-14 14:23:19 +0530

    Added Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
 import { Container } from "react-bootstrap";
 import "./App.css";
+import Footer from "./components/footer";
+import Header from "./components/header";
 
 function App() {
   return (
     <>
-      {/* <div className="container">
+      <Header />
+      <main>
+        {/* <div className="container">
         <h1>E-Commerce App</h1>
       </div> */}
-      {/* is equivalent to below */}
-      <Container>
-        <h1>E-Commerce App</h1>
-      </Container>
-      <h1>E-Commerce App</h1>
+        {/* is equivalent to below */}
+        <Container>
+          <h1>E-Commerce App</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </>
   );
 }
