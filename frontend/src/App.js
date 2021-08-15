f805ed5144ddd55b3d58a80b176b4a3875d9e23e
Raj <rajsanghavi9@gmail.com>
2021-08-15 12:50:25 +0530

    Created Page for displaying a single product description

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductDetails from "./screens/ProductDetails";
 
 function App() {
   return (
@@ -16,11 +17,12 @@ function App() {
       </div> */}
         {/* is equivalent to below */}
         <Container>
-          <h1>E-Commerce App</h1>
+          {/* <h1>E-Commerce App</h1> */}
           {/* <HomeScreen /> */}
           {/* Same as below */}
           {/* exact => path must be exactly same */}
           <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/products/:id" component={ProductDetails} exact></Route>
         </Container>
       </main>
 
---
+++ frontend/src/screens/ProductDetails.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Product from "../products";
+import {
+  ListGroup,
+  Image,
+  Row,
+  Col,
+  ListGroupItem,
+  Button,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "../components/rating";
+
+const ProductDetails = ({ match }) => {
+  const product = Product.find((p) => p._id === match.params.id);
+
+  return (
+    <div>
+      <Link to="/" className="btn btn-dark my-3">
+        <i className="fas fa-arrow-left"></i>&nbsp;Go back to Home Screen
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h3>{product.name}</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={` from ${product.numReviews} reviews`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>Price : ${product.price}</ListGroupItem>
+            <ListGroupItem>{product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <ListGroupItem>
+            <Row>
+              <Col>Status</Col>
+              {product.countInStock > 0 ? (
+                <Col className={"success-stock-color"}>In Stock</Col>
+              ) : (
+                <Col className={"not-success-stock-color"}>Out of Stock</Col>
+              )}
+            </Row>
+          </ListGroupItem>
+          <ListGroupItem className="d-grid">
+            {/* Button type="btn-block" and no className above */}
+            <Button type="button">Addd to cart</Button>
+          </ListGroupItem>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default ProductDetails;
