f0a02114983e1cee8cf3cc594c6d6fe51d59d97a
Raj <rajsanghavi9@gmail.com>
2021-08-16 12:58:25 +0530

    Setting Enviroment Variables

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,16 @@
 const express = require("express");
-const products = require("./data/products");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+const products = require("./data/products");
 
 const app = express();
-// app.use(allowCrossDomain);
-// app.use(cors());
+
+dotenv.config();
+const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
+
 app.use(cors());
-
-// app.use(function (req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-
-//   next();
-// });
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to node server</h1>");
@@ -35,6 +25,6 @@ app.get("/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080.");
+app.listen(PORT, () => {
+  console.log(`Server is running in ${NODE_ENV} on port ${PORT}.`);
 });
