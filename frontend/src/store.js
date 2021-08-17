3d8ce9d06ae5dd2b4e09bcc2f17a317bcacb49ff
Raj <rajsanghavi9@gmail.com>
2021-08-17 15:39:25 +0530

    Created Redux store object

---
+++ frontend/src/store.js
@@ -0,0 +1,25 @@
+// To manage global state
+import { createStore, combineReducers, applyMiddleware } from "redux";
+// To send data from 1 component to another component
+import thunk from "redux-thunk";
+// To get an option of viewing redu from browseer
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// get all reducers in a single variable using combineReducers
+const reducer = combineReducers({});
+
+// Initial State
+const initialState = {};
+
+// communicates between react and redux
+const middleware = [thunk];
+
+// creating store object
+const store = createStore(
+  reducer,
+  initialState,
+  //   Removed square brackets
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
