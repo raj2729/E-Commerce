5b7c20bdce9d73586b0ce04324bd6f9bacca4873
Raj <rajsanghavi9@gmail.com>
2021-08-23 22:54:16 +0530

    Created route for getting Order by id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -39,4 +39,18 @@ const addOrderItem = asyncHandler(async (req, res) => {
   // }
 });
 
-module.exports = { addOrderItem };
+// Get order by Id
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+module.exports = { addOrderItem, getOrderById };
--- backend/routes/ordersRoute.js
+++ backend/routes/ordersRoute.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { addOrderItem } = require("../controllers/orderController");
+const {
+  addOrderItem,
+  getOrderById,
+} = require("../controllers/orderController");
 const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 // Create new order
 router.route("/").post(protect, addOrderItem);
+
+// Get order by Id
+router.route("/:id").get(protect, getOrderById);
 
 module.exports = router;
